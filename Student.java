7e0d0adee92cd3f5fb40389ac111c515e334068e
vishvnathp <vishvnathp6388@gmail.com>
2022-12-21 10:14:47 +0530

    My First Commit

---
+++ Student.java
@@ -0,0 +1,41 @@
+package Aggregation;
+
+import Aggregation.Address;
+import Aggregation.Emp;
+
+class Address {
+	String city, state, con;
+
+	public Address(String city, String state, String con) {
+		this.city = city;
+		this.state = state;
+		this.con = con;
+
+	}
+
+}
+
+class Emp extends Address {
+
+	String name;
+
+	public Emp(String city, String state, String con, String name)
+
+	{
+		super(city, state, con);
+		this.name = name;
+
+	}
+
+	public void dis() {
+		System.out.println(name);
+		System.out.println(city + " " + state + " " + con);
+	}
+}
+
+public class Student {
+	public static void main(String args[]) {
+		Emp add = new Emp("kanpur", "up", "india", "Anuj");
+		add.dis();
+	}
+}
